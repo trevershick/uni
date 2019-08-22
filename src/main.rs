74104c7328ce3a2620e7b3b545a01ddbce5e041d
Trever Shick <trever.shick@tanium.com>
2019-08-21 23:32:45 -0400

    Use Clap for argument processing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-use hex;
-use std::env;
+#[macro_use]
+extern crate simple_error;
+#[macro_use]
+extern crate clap;
+
 use std::error::Error;
 use std::process::exit;
 use std::result::Result;
 
-#[macro_use]
-extern crate simple_error;
+use clap::App;
+use hex;
 
 fn utf8_to_utf16(unicode_bytes: Vec<u8>) -> Result<String, Box<dyn Error>> {
     if unicode_bytes.len() != 2 {
@@ -15,31 +18,30 @@ fn utf8_to_utf16(unicode_bytes: Vec<u8>) -> Result<String, Box<dyn Error>> {
     let mut wide: u16 = unicode_bytes[0] as u16;
     wide <<= 8;
     wide += unicode_bytes[1] as u16;
-    match String::from_utf16(&[wide]) {
+    return match String::from_utf16(&[wide]) {
         Ok(v) => Ok(v),
         Err(x) => Err(Box::new(x)),
-    }
+    };
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let matches = App::new("uni")
+        .version("1.0.0")
+        .about("Convert unicode hex to unicode character")
+        .args_from_usage("<hex_vals>... 'A sequence of utf16 hex values, i.e. 30CE B0AB'")
+        .get_matches();
 
-    let (_, hex_values) = args.split_first().unwrap();
-    if hex_values.len() < 1 {
-        eprintln!("Need at least 1 argument");
-        exit(1);
-    }
+    let hex_values = values_t!(matches, "hex_vals", String).unwrap();
 
     let mut bad: Vec<String> = Vec::new();
-
     for hex_value in hex_values {
-        let decoded = hex::decode(hex_value);
+        let decoded = hex::decode(&hex_value);
         match decoded {
             Ok(unicode_bytes) => match utf8_to_utf16(unicode_bytes) {
                 Ok(v) => print!("{}", v),
-                Err(_) => bad.push(String::from(hex_value)),
+                Err(_) => bad.push(hex_value),
             },
-            Err(_) => bad.push(String::from(hex_value)),
+            Err(_) => bad.push(hex_value),
         };
     }
 
